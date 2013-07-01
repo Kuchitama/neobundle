114d51b8f7654185db1d721c92247ab7630176cb
kuchitama <kiyo.kunihira@gmail.com>
2013-07-01 15:25:37 +0200

    add a template of .vimrc

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,7 +19,7 @@ directory BUNDLE_HOME do
 	recursive true
 end
 
-git BUNDLE_HOME do
+git BUNDLE_HOME << "/neobundle.vim" do
 	user USER
 	group GROUP
 	repository "git://github.com/Shougo/neobundle.vim"
@@ -30,4 +30,12 @@ end
 bash "chown" do
 	cwd BUNDLE_HOME
 	code "chown -R " <<  USER << ":" << GROUP << " " << BUNDLE_HOME << "&&" << "chmod -R 744 " << BUNDLE_HOME
+end
+
+template ".vimrc" do
+	path node["neobundle"]["vim_home"] << "/../.vimrc"
+	source "vimrc.erb"
+	owner USER
+	group GROUP
+	mode 00744
 end
